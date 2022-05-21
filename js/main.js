16b2ca6215ac5132e163b0d20d9bf49cb96e2ea1
agao1318 <agao1318@gmail.com>
2022-05-21 08:27:25 +0200

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let firstNumber = 12
 let secondNumber = 14
-function add (firstNuber, secondNumber) {
-    let result = firstNumber + secondNumber;
+function add (firstNumber, secondNumber) {
+    let result = 12 + 14;
     return result; 
 }
  console.log(add(12, 14) );
